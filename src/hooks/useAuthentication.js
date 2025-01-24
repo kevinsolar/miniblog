5d024b5c0dd8c12c7d5ec3bd59bd89c760fbb354
Kevin BTA <kevinleitesolar@hotmail.com>
2025-01-24 16:33:54 -0300

    Criado o hook de cadastro para o blog, fizemos a finalização da página de cadastros com os estados de cada item e também a validação de cadastro.

---
+++ src/hooks/useAuthentication.js
@@ -0,0 +1,91 @@
+import { db } from "../firebase/config";
+
+//Importando funções que iremos precisar.
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	updateProfile, //Para atualizarmos o perfil do usuario.
+	signOut,
+} from "firebase/auth";
+// Primeiro utilizaremos a criação do usuario para depois atualizarmos o perfil do usuario
+
+import { useState, useEffect } from "react";
+
+export const useAuthentication = () => {
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(null);
+
+	//cleanup
+	//deal with memory leak -> memory leak é quando temos o problema de mudar de página ou algo do tipo e ainda mantermos as informações que tinhamos preenchido ou enviado em algum momento anterior/ pagina anterior, mas que não era pra acontecer.
+	const [cancelled, setCancelled] = useState(false);
+
+	const auth = getAuth(); // Isso não significa que o usuario está autenticado, mas que posso utilizar ele para realizar funções de autenticação a partir dele.
+
+	function checkIfIsCancelled() {
+		if (cancelled) {
+			return;
+		}
+	}
+
+	const createUser = async (data) => {
+		checkIfIsCancelled();
+		//Criamos a função de cleanup antes, aqui utilizamos ela para fazer seu papel.
+
+		setLoading(true);
+		setError(null);
+
+		//Fazemos o try-catch para tentar criar o usuario, caso não der certo, validamos os erros.
+		try {
+			//Aqui pegamos o usuario que chega da função
+			const { user } = await createUserWithEmailAndPassword(
+				auth,
+				data.email,
+				data.password
+			);
+
+			//Após receber o usuário, então vamos atualizar o perfil do usuario
+			await updateProfile(user, {
+				displayName: data.displayName,
+				//* Podemos passar o displayName e o photoURL
+			});
+
+			//Ao finalizar a função, colocamos o setLoading para falso, pois já terminou de carregar/realizar a função.
+			setLoading(false);
+
+			//retorno o usuario
+			return user;
+		} catch (error) {
+			console.log(error.message);
+			console.log(typeof error.message);
+
+			let systemErrorMessage;
+			if (error.message.includes("Password")) {
+				systemErrorMessage =
+					"A senha precisa conter pelo menos 6 caracteres.";
+			} else if (error.message.includes("email-already")) {
+				systemErrorMessage = "E-mail já cadastrado.";
+			} else {
+				systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+			}
+
+			//Ao finalizar a função, colocamos o setLoading para falso, pois já terminou de carregar/realizar a função.
+			setLoading(false);
+         // Caso tenha erro e de acordo com o erro, passaremos a mensagem referente ao erro.
+			setError(systemErrorMessage);
+		}
+	};
+
+	//Utilizamos esse useEffect com o plano de que ele execute uma vez só, para colocar o cancelado como true, assim que sairmos da página.
+	useEffect(() => {
+		return () => setCancelled(true);
+	}, []);
+
+	//Criamos então o retorno das coisas que criamos
+	return {
+		auth,
+		createUser,
+		error,
+		loading,
+	};
+};
--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,14 +1,51 @@
+import { useAuthentication } from "../../hooks/useAuthentication";
 import styles from "./Cadastro.module.css";
 
 import { useState, useEffect } from "react";
 
 const Cadastro = () => {
+	const [displayName, setDisplayName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [error, setError] = useState("");
+
+   const { createUser, error: authError, loading } = useAuthentication();//renomeamos o error, pois já temos um error aqui no frontend.
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		setError("");
+
+		const user = {
+			displayName,
+			email,
+			password,
+		};
+
+		if (password !== confirmPassword) {
+			setError("As senhas precisam ser iguais!");
+
+			return;
+		}
+
+      const res = await createUser(user)
+
+		console.log(res);
+	};
+
+   useEffect(() => {
+      if(authError) {
+         setError(authError);
+      }
+   }, [authError])
+
 	return (
 		<section id="cadastro">
 			<div className="title">
 				<h1>Cadastre-se para postar</h1>
 			</div>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label>
 					<span>Nome</span>
 					<input
@@ -16,11 +53,20 @@ const Cadastro = () => {
 						name="displayName"
 						required
 						placeholder="Nome de usuário"
+						value={displayName}
+						onChange={(e) => setDisplayName(e.target.value)}
 					/>
 				</label>
 				<label>
 					<span>E-mail</span>
-					<input type="email" name="email" required placeholder="E-mail" />
+					<input
+						type="email"
+						name="email"
+						required
+						placeholder="E-mail"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
 				</label>
 				<label>
 					<span>Senha</span>
@@ -29,19 +75,27 @@ const Cadastro = () => {
 						name="password"
 						required
 						placeholder="Crie uma senha"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
 					/>
 				</label>
 				<label>
 					<span>Confirme sua senha</span>
 					<input
 						type="password"
-						name="confrimPassword"
+						name="confirmPassword"
 						required
 						placeholder="Confirme sua senha"
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
 					/>
 				</label>
 
-				<button className="btn">Cadastrar</button>
+
+				{!loading && <button className="btn">Cadastrar</button>}
+            {loading && <button className="btn" disabled>Aguarde...</button>}
+
+            {error && <p className="error">{error}</p>}
 			</form>
 		</section>
 	);
