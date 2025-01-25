125082a9f78de5870e40d717f05c3a2f15b7a84b
Kevin Solar <kevinleitesolar@hotmail.com>
2025-01-25 18:23:32 -0300

    Implementação do contexto de autenticação e gerenciamento de estado do usuário no App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { onAuthStateChanged } from "firebase/auth";//Essa importacao que mapeia se a autenticacao do usario ocorreu tudo certo.
+
+//HOOKS
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
+//CONTEXT
+import { AuthProvider } from "./context/AuthContext";
+
+//PAGES
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import Navbar from "./components/Navbar";
@@ -9,22 +19,41 @@ import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
 
 function App() {
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Navbar />
-				<div className="container">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/sobre" element={<Sobre />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/cadastro" element={<Cadastro />} />
-					</Routes>
-				</div>
-				<Footer />
-			</BrowserRouter>
-		</div>
-	);
+
+   const [user, setUser] = useState(undefined)//Comecando sem usuario com undefined
+   const { auth } = useAuthentication();//chamamos nosso auth do useAuth... para nao precisar fazer novamente aqui.
+
+   const loadingUser = user === undefined
+   // Atribuo ao estado de loading do usuario a comparacao de user com undefined, entao se for undefined ele esta carregando de alguma maneira. Evita de mostrar algo antes que o usuario esteja realmente logado.
+
+   useEffect(() => {
+      onAuthStateChanged(auth, (user) => {
+         setUser(user)
+      })
+   }, [auth])
+
+   if (loadingUser) {
+      return <p>Carregando...</p>;
+   }
+
+   return (
+      <div className="App">
+         <AuthProvider value={{ user }}>
+            <BrowserRouter>
+               <Navbar />
+               <div className="container">
+                  <Routes>
+                     <Route path="/" element={<Home />} />
+                     <Route path="/sobre" element={<Sobre />} />
+                     <Route path="/login" element={<Login />} />
+                     <Route path="/cadastro" element={<Cadastro />} />
+                  </Routes>
+               </div>
+               <Footer />
+            </BrowserRouter>
+         </AuthProvider>
+      </div>
+   );
 }
 
 export default App;
