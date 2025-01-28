8074f7e6030bb8767f59ff6452ea5f8250737373
Kevin BTA <kevinleitesolar@hotmail.com>
2025-01-28 17:34:50 -0300

    Criando a página de criação e post - parte 1 na parte 2 pretendo estilizar melhor.

--- src/pages/CreatePost/index.jsx
+++ src/pages/CreatePost/index.jsx
@@ -1,9 +1,82 @@
-import styles from './CreatePost.module.css';
+import styles from "./CreatePost.module.css";
+
+import { useState } from "react";
 
 const CreatePost = () => {
-   return (
-      <div>Create Post</div>
-   )
-}
+	const [title, setTitle] = useState("");
+	const [img, setImg] = useState("");
+	const [body, setBody] = useState("");
+	const [tags, setTags] = useState("");
+	const [formError, setFormError] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<section id="postar">
+			<div className="title">
+				<h1>Criar post</h1>
+				<p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+			</div>
+
+			<form onSubmit={handleSubmit}>
+				<label>
+					<span>Titulo:</span>
+					<input
+						type="text"
+						name="title"
+						required
+						placeholder="Pense num bom titulo"
+						onChange={(e) => setTitle(e.target.value)}
+						value={title}
+					/>
+				</label>
+				<label>
+					<span>link/url da imagem:</span>
+					<input
+						type="text"
+						name="image"
+						required
+						placeholder="Insira o link da imagem que você quer para seu post"
+						onChange={(e) => setImg(e.target.value)}
+						value={img}
+					/>
+				</label>
+				<label>
+					<span>Conteudo:</span>
+					<textarea
+						name="body"
+						required
+						placeholder="Insira o conteudo do post"
+						onChange={(e) => setBody(e.target.value)}
+						value={body}
+					></textarea>
+				</label>
+				<label>
+					<span>Tags:</span>
+					<input
+						type="text"
+						name="tags"
+						required
+						placeholder="Insira as Tags para o post, separadas por virgula."
+						onChange={(e) => setTags(e.target.value)}
+						value={tags}
+					/>
+				</label>
+
+            <button className="btn">Postar</button>
+				{/* {!loading && <button className="btn">Postar</button>}
+				{loading && (
+					<button className="btn" disabled>
+						Aguarde...
+					</button>
+				)}
+
+				{error && <p className="error">{error}</p>} */}
+			</form>
+		</section>
+	);
+};
 
 export default CreatePost;
