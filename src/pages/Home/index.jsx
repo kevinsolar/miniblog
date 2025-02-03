b3d9fb2bef726664e3ebfad3615b101692a30e2b
Kevin BTA <kevinleitesolar@hotmail.com>
2025-02-03 16:56:58 -0300

    Fazendo o front-end inicial da Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,44 @@
-import styles from './Home.module.css';
+import styles from "./Home.module.css";
+
+//Hooks
+import { useNavigate, Link } from "react-router";
+import { useState } from "react";
 
 const Home = () => {
-   return (
-      <div>
-         <h1>Home</h1>
-      </div>
-   )
-}
+	const [query, setQuery] = useState("");
+	const [posts] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<section id="home" className={styles.home}>
+			<h1 className={styles.title}>Veja os nossos posts mais recentes</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="Ou busque por tags..."
+					onChange={(e) => setQuery(e.target.value)}
+					value={query}
+				/>
+				<button type="submit" className="btn btn-dark">
+					Pesquisar
+				</button>
+			</form>
+			<div>
+				<h1>Posts:</h1>
+				{posts && posts.length === 0 && (
+					<div className={styles.not_found}>
+						<p>Não foram encontrados posts</p>
+						<Link to="/posts/create" className="btn">
+							Criar um post
+						</Link>
+					</div>
+				)}
+			</div>
+		</section>
+	);
+};
 
 export default Home;
