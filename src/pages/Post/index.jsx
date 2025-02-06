2c0e2d2a2e632a3cf73c031f66da21b147ae10db
Kevin Solar <kevinleitesolar@hotmail.com>
2025-02-06 20:57:53 -0300

    Adiciona estilização e estrutura para a página de postagens

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,23 +1,42 @@
+import styles from './Posts.module.css'
+
 //hooks
 import { useParams } from "react-router";
 import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 const Post = () => {
-	const { id } = useParams();
-	const { document: post, loading } = useFetchDocument("posts", id);
+   const { id } = useParams();
+   const { document: post, loading } = useFetchDocument("posts", id);
 
-	return (
-		<div>
+   return (
+      <div className={styles.post_container}>
          {loading && (
             <h1>Carregando...</h1>
          )}
-			{post && (
-				<>
-					<h1>{post.title}</h1>
-				</>
-			)}
-		</div>
-	);
+         {post && (
+            <>
+               <div className={styles.banner}>
+                  <img src={post.img} alt={post.title} />
+               </div>
+               <div className={styles.title}>
+                  <h1>{post.title}</h1>
+               </div>
+               <div className={styles.content}>
+                  <p>{post.body}</p>
+                  <h3>Tags:</h3>
+                  <div className={styles.tags}>
+                     {post.tagsArray.map((tag) => (
+                        <p key={tag}>
+                           <span>#</span>
+                           {tag}
+                        </p>
+                     ))}
+                  </div>
+               </div>
+            </>
+         )}
+      </div>
+   );
 };
 
 export default Post;
