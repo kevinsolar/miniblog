9e71057cbc4c80e629628521eb51d3f22151545a
Kevin BTA <kevinleitesolar@hotmail.com>
2025-02-07 17:42:43 -0300

    Criando estrutura inicial para Dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,9 +1,34 @@
-import styles from './Dashboard.module.css';
+import styles from "./Dashboard.module.css";
+
+import { Link } from "react-router";
+
+//hooks
+import { useAuthValue } from "../../context/AuthContext";
+// import { useFethDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
-   return (
-      <div>Dashboard</div>
+   const { user } = useAuthValue()
+   const uid = user.uid
+
+   //posts do usuario
+   const posts = []
+
+	return (
+      <div>
+         <h2>Dashboard</h2>
+         <p>Gerencie os seus posts</p>
+         {posts && posts.length === 0 ? (
+            <div>
+               <p>Não foram encontrados posts</p>
+               <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+            </div>
+         ) : (
+            <div>
+               <p>Tem posts!</p>
+            </div>
+         )}
+      </div>
    )
-}
+};
 
 export default Dashboard;
